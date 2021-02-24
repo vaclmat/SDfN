4d5cabd9f160314fc25d472ab394469399605466
Vaclav Matousek <vaclmat@email.cz>
2021-02-24 17:01:40 +0100

    Initial Commit

---
+++ SDfN/SDfNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDfNApp.swift
+//  SDfN
+//
+//  Created by Václav Matoušek on 24.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SDfNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
